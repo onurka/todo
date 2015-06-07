d537e5be8794179a7937477c9e7e977b4be43953
Onur Karaagaoglu <onurka@onurka.com>
2015-06-06 18:17:15 -0700

    enforce model validation in list view

--- lists/views.py
+++ lists/views.py
@@ -1,24 +1,25 @@
 from django.core.exceptions import ValidationError
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
-
 from lists.models import Item, List
 
-# Create your views here.
-
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-    
     return render(request, 'home.html')
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
+    
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect('/lists/%d/' % (list_.id,))
-    return render(request, 'list.html', {'list': list_})
+        try:
+            #item = Item.objects.create(text=request.POST['item_text'], list=list_)
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect('/lists/%d/' % (list_.id,))
+        except ValidationError:
+            error = "You can't have an empty list item"
+            
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
 def new_list(request):
     list_ = List.objects.create()
@@ -31,10 +32,3 @@ def new_list(request):
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
     return redirect('/lists/%d/' % (list_.id,))
-
-"""
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect('/lists/%d/' % (list_.id,))
-"""
